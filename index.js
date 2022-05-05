89bbafa912bd3246270d875973447000e95d8a20
DanielTibaes505 <daniel.tibaes@outlook.com>
2022-05-05 18:04:23 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function onScroll () {
+  let nav = document.querySelector('#navigation');
+
+  if(scrollY == 0){
+    nav.classList.remove('scroll');
+
+  }else{
+    nav.classList.add('scroll');
+  }
+}
+function openMenu(){
+  document.body.classList.add('menu-expanded');
+}
+function closeMenu(){
+  document.body.classList.remove('menu-expanded');
+}
